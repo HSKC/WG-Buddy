e59ca5cce664719951919bcb0f3245010a9e35c6
Dominik <Dominik_Wieland@gmx.de>
2012-05-20 16:35:34 +0200

    Activities eingebaut

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/WGBuddyActivity.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/WGBuddyActivity.java
@@ -1,13 +1,53 @@
 package de.htwg.lpn.wgbuddy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class WGBuddyActivity extends Activity {
-    /** Called when the activity is first created. */
+public class WGBuddyActivity extends Activity 
+{
+
+	private Button shoppinglist;
+	private Button taskdistributor;
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        shoppinglist = (Button) findViewById(R.id.shoppingListButton);
+        taskdistributor = (Button) findViewById(R.id.taskDistributorButton);
+        
+        shoppinglist.setOnClickListener
+        (
+    		new OnClickListener() 
+    		{
+				
+				@Override
+				public void onClick(View v) 
+				{
+					Intent intent = new Intent(WGBuddyActivity.this,ShoppingList.class);
+					startActivity(intent);
+				}
+    		}
+        );
+        
+        taskdistributor.setOnClickListener
+        (
+    		new OnClickListener() 
+    		{
+				
+				@Override
+				public void onClick(View v) 
+				{
+					Intent intent = new Intent(WGBuddyActivity.this,TaskDistributor.class);
+					startActivity(intent);
+				}
+    		}
+        );
     }
 }
