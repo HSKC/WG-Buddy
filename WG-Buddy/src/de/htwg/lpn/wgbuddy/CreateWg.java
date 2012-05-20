0809e2bdcb0df5d84f23e61e5fa4314dac87d557
Dominik <Dominik_Wieland@gmx.de>
2012-05-21 01:40:15 +0200

    Addes WG create and Admin

---
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/CreateWg.java
@@ -0,0 +1,14 @@
+package de.htwg.lpn.wgbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateWg extends Activity 
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.createwg);
+    }
+}
