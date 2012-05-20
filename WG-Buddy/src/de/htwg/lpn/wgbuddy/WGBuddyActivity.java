a5e3329bd509f1f762741192d29151000fa2a1bb
Dominik <Dominik_Wieland@gmx.de>
2012-05-20 20:30:18 +0200

    Loginstuff

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/WGBuddyActivity.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/WGBuddyActivity.java
@@ -2,6 +2,7 @@ package de.htwg.lpn.wgbuddy;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,7 +10,8 @@ import android.widget.Button;
 
 public class WGBuddyActivity extends Activity 
 {
-
+	public static final String PREFS_NAME = "WGBuddyPreferences";
+	
 	private Button shoppinglist;
 	private Button taskdistributor;
 	
@@ -17,7 +19,20 @@ public class WGBuddyActivity extends Activity
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        if(settings.contains("initiated"))
+        {
+        	//Applikation schon initialisiert. Lade Einstellungen
+        	setContentView(R.layout.main);
+        }
+        else
+        {
+        	Intent intent = new Intent(WGBuddyActivity.this,Preferences_WG.class);
+			startActivity(intent);
+			return;
+        }
         
         shoppinglist = (Button) findViewById(R.id.shoppingListButton);
         taskdistributor = (Button) findViewById(R.id.taskDistributorButton);
