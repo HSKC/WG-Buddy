049d53063ebf4e90b77150545e1ee3803d073b7d
Dominik <Dominik_Wieland@gmx.de>
2012-05-20 14:46:00 +0200

    initial

---
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/WGBuddyActivity.java
@@ -0,0 +1,13 @@
+package de.htwg.lpn.wgbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WGBuddyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
