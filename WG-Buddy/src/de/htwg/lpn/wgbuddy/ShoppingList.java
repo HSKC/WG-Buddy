28c3a9aca66a613effe97c1b73cd7943af451049
Dominik <Dominik_Wieland@gmx.de>
2012-05-21 23:56:27 +0200

    spielereien mit der Shoppingliste

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
@@ -1,13 +1,18 @@
 package de.htwg.lpn.wgbuddy;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ShoppingList extends Activity 
 {
-	private View view;
+	private LinearLayout shoppingList;
 	
 	 @Override
 	    public void onCreate(Bundle savedInstanceState) 
@@ -15,6 +20,26 @@ public class ShoppingList extends Activity
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.shoppinglist);
 	        
-	        view = findViewById(R.id.shoppingListView);
+	        shoppingList = (LinearLayout) findViewById(R.id.shoppinglist);
+	        
+	        ArrayList<String> items= new ArrayList<String>();
+	        
+	        items.add("Bier");
+	        items.add("Mehr Bier");
+	        items.add("Baconstrips");
+	        
+	        for (String string : items) 
+	        {
+	        	View view = getLayoutInflater().inflate(R.layout.shoppinglistentry, shoppingList, false);
+		        
+	        	((TextView)view.findViewById(R.id.shoppingBigText)).setText(string);
+	        	
+		        shoppingList.addView(view);
+
+			}
+	        
+
+	        
+	        
 	    }
 }
