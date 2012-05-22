5a5778c7c44af1b4285afb5f4600c89e965f8e31
Jens <jens.kueblbeck@googlemail.com>
2012-05-23 01:43:22 +0200

    Model hinzugefügt

---
+++ WG-Buddy/src/de/htwg/lpn/model/Message.java
@@ -0,0 +1,99 @@
+package de.htwg.lpn.model;
+
+import java.sql.Date;
+
+public class Message
+{
+	private Integer id;
+	private Integer wgId;
+	private Integer userId;
+	private String title;
+	private String message;
+	private Date createdDate;
+	private Integer status;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the wgId
+	 */
+	public Integer getWgId() {
+		return wgId;
+	}
+	/**
+	 * @param wgId the wgId to set
+	 */
+	public void setWgId(Integer wgId) {
+		this.wgId = wgId;
+	}
+	/**
+	 * @return the userId
+	 */
+	public Integer getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	/**
+	 * @return the status
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+}
