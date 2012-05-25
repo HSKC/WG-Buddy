0cca57e9aeefa7c45757dc271c5ad57cfe4e684f
Jens <jens.kueblbeck@googlemail.com>
2012-05-25 15:40:30 +0200

    - ShoppingList: SetStatus = 1

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
@@ -2,6 +2,10 @@ package de.htwg.lpn.wgbuddy;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import de.htwg.lpn.model.ShoppingItem;
 import android.app.Activity;
@@ -27,6 +31,8 @@ public class ShoppingList<T> extends Activity
 	private ListView shoppingList;
 	private View addButton;
 	private View refreshButton;
+	
+	private ImageButton completedButton;
 	private ImageButton deleteButton;	
 	
 	 @Override
@@ -41,7 +47,7 @@ public class ShoppingList<T> extends Activity
 	        
 	        ArrayList<HashMap<String, String>> list = JSONStuff.getMapListOfJsonArray( "http://wgbuddy.domoprojekt.de/shopping.php", "Item"); 
 	        
-	        SimpleAdapter sa = new SimpleAdapter(this, list, R.layout.shoppinglist_entry, new String[] { "id", "name", "comment", "rating", "createdDate" }, new int[] { R.id.shoppingListEntryDeleteButton, R.id.shoppingBigText, R.id.shoppingSmallText, R.id.ratingBar, R.id.createdDate});
+	        SimpleAdapter sa = new SimpleAdapter(this, list, R.layout.shoppinglist_entry, new String[] { "id", "id", "name", "comment", "rating", "createdDate" }, new int[] { R.id.shoppingListEntryCompletedButton, R.id.shoppingListEntryDeleteButton, R.id.shoppingBigText, R.id.shoppingSmallText, R.id.ratingBar, R.id.createdDate});
 	        
 	        ViewBinder vb = new ViewBinder() 
 	        {
@@ -53,6 +59,30 @@ public class ShoppingList<T> extends Activity
 					{
 						RatingBar rb = (RatingBar) view;
 						rb.setRating(Integer.valueOf(data.toString()));
+						return true;
+					}
+					else if(view.getId() == R.id.shoppingListEntryCompletedButton)
+					{
+						ImageButton button = (ImageButton) view;
+						Integer id = Integer.valueOf(data.toString());
+						button.setTag(id);
+						button.setOnClickListener(new OnClickListener() 
+						{
+							
+							@Override
+							public void onClick(View v) 
+							{
+								ShoppingItem si = new ShoppingItem(store);
+								Integer id = (Integer) v.getTag();
+								
+								List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+								nameValuePairs.add(new BasicNameValuePair("status", "1"));
+										
+								si.update(id, nameValuePairs);
+								
+								refreshButton.performClick();
+							}
+						});
 						return true;
 					}
 					else if(view.getId() == R.id.shoppingListEntryDeleteButton)
