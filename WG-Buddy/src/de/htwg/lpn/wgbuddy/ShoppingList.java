16840b739db30784cec1021c7d710aa9358d8aca
Jens <jens.kueblbeck@googlemail.com>
2012-05-25 15:01:04 +0200

    -Delete ShoppingList Entry

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/ShoppingList.java
@@ -10,15 +10,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleAdapter.ViewBinder;
 
 
-public class ShoppingList extends Activity 
+public class ShoppingList<T> extends Activity 
 {
 	private Store store;
 	
@@ -39,8 +41,7 @@ public class ShoppingList extends Activity
 	        
 	        ArrayList<HashMap<String, String>> list = JSONStuff.getMapListOfJsonArray( "http://wgbuddy.domoprojekt.de/shopping.php", "Item"); 
 	        
-
-	        SimpleAdapter sa = new SimpleAdapter(this, list, R.layout.shoppinglist_entry, new String[] { "name", "comment", "rating", "createdDate" }, new int[] { R.id.shoppingBigText, R.id.shoppingSmallText, R.id.ratingBar, R.id.createdDate});
+	        SimpleAdapter sa = new SimpleAdapter(this, list, R.layout.shoppinglist_entry, new String[] { "id", "name", "comment", "rating", "createdDate" }, new int[] { R.id.shoppingListEntryDeleteButton, R.id.shoppingBigText, R.id.shoppingSmallText, R.id.ratingBar, R.id.createdDate});
 	        
 	        ViewBinder vb = new ViewBinder() 
 	        {
@@ -54,12 +55,26 @@ public class ShoppingList extends Activity
 						rb.setRating(Integer.valueOf(data.toString()));
 						return true;
 					}
-//					else if(view.getId() == R.id.deleteButton)
-//					{
-//						ImageButton button = (ImageButton) view;
-//						button.setTag(Integer.valueOf(data.toString()));
-//						return true;
-//					}
+					else if(view.getId() == R.id.shoppingListEntryDeleteButton)
+					{
+						ImageButton button = (ImageButton) view;
+						Integer id = Integer.valueOf(data.toString());
+						button.setTag(id);
+						button.setOnClickListener(new OnClickListener() 
+						{
+							
+							@Override
+							public void onClick(View v) 
+							{
+								ShoppingItem si = new ShoppingItem(store);
+								Integer id = (Integer) v.getTag();
+								si.delete(id);
+								
+								refreshButton.performClick();
+							}
+						});
+						return true;
+					}
 					else
 					{
 						return false;
@@ -67,22 +82,7 @@ public class ShoppingList extends Activity
 				}
 			};
 			sa.setViewBinder(vb);
-	        shoppingList.setAdapter(sa);
-	        
-	        shoppingList.setClickable(true);
-	        
-	        shoppingList.setOnItemClickListener(new AdapterView.OnItemClickListener() 
-	        {
-
-				@Override
-				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) 
-				{
-					Integer id = (Integer) arg1.getTag();
-					ShoppingItem si = new ShoppingItem(store);
-					si.delete(id);
-				}
-			});
-	
+	        shoppingList.setAdapter(sa);	
 	        
 	        addButton = (Button) findViewById(R.id.shoppinglistAddButton);
 	        refreshButton = (Button) findViewById(R.id.shoppinglistRefreshButton);
