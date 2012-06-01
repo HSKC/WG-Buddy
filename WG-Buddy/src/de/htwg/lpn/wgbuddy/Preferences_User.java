2b9e35dbc31122df6fa9ea20b2f80b07746001a3
Jens <jens.kueblbeck@googlemail.com>
2012-06-01 02:19:27 +0200

    Refactor

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/Preferences_User.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/Preferences_User.java
@@ -17,6 +17,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.text.method.SingleLineTransformationMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -150,6 +153,8 @@ public class Preferences_User extends Activity
 		AlertDialog.Builder builder = new AlertDialog.Builder(Preferences_User.this);
 		
 		final EditText editText = new EditText(Preferences_User.this);
+		editText.setTransformationMethod(SingleLineTransformationMethod.getInstance());
+		editText.setHint("E-Mail");
 		
 		builder.setMessage("Bitte gebe deine E-Mail Adresse ein. Ein zufällig generierter  Schlüssel wird dann an deine E-Mial Adresse geschickt.");
 		builder.setCancelable(true);
@@ -182,8 +187,17 @@ public class Preferences_User extends Activity
 		AlertDialog.Builder builder = new AlertDialog.Builder(Preferences_User.this);
 		
 		final EditText keyEditText = new EditText(Preferences_User.this);
-		final EditText passwordEditText = new EditText(Preferences_User.this);
+		final EditText passwordEditText = new EditText(Preferences_User.this);		
 		final EditText password2EditText = new EditText(Preferences_User.this);
+		
+		keyEditText.setTransformationMethod(SingleLineTransformationMethod.getInstance());
+		keyEditText.setHint("Schlüssel");
+		
+		passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+		passwordEditText.setHint("Passwort");
+		
+		password2EditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+		password2EditText.setHint("Passwort wiederholen");
 		
 		final LinearLayout layout = new LinearLayout(Preferences_User.this);
 		layout.setOrientation(LinearLayout.VERTICAL);
