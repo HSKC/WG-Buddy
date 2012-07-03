a54377a88142d380a844ad63e2b411cd9ac886a3
felix <felix@felix-PC.localdomain>
2012-07-03 12:41:50 +0200

    bla

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/TaskDistributor.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/TaskDistributor.java
@@ -48,45 +48,31 @@ public class TaskDistributor extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.taskdistributor);
-        settings = getSharedPreferences(WGBuddyActivity.PREFS_NAME, 0);
         
-        userList = (ListView) findViewById(R.id.taskUserList);
-        start = (Button) findViewById(R.id.taskGoButton);
-        start.setOnClickListener(new OnClickListener() 
-        {
-			
-			@Override
-			public void onClick(View v) 
-			{			
-				ListAdapter adapter = userList.getAdapter();				
-				Integer count = adapter.getCount();
-				TreeMap<String, Double> checkedUser = new TreeMap<String, Double>();
-				
-				for(int i = 0; i < count; i++)
-				{
-					View entry = userList.getChildAt(i);
-					CheckBox checkbox = (CheckBox) entry.findViewById(R.id.task_userlistcheckbox);
-					if(checkbox.isChecked())
-					{						
-						HashMap<String, String> userData = (HashMap<String, String>) adapter.getItem(i);
-						checkedUser.put(userData.get("username"), Double.valueOf(userData.get("points")));
-					}
-				}
-				
-				RandomUser rand = RandomUser.getInstance();
-				rand.setUserlist(checkedUser);
-				
-				Intent intent = new Intent(TaskDistributor.this, Create_Task.class);
-				startActivity(intent);				
-			}
-
-			
-		});
+        init();
         
-        user = new User(settings); 
-        users = user.get("?wgId=" + settings.getString("wg_id", ""));
+        clickOnStartButton();
    
-	    SimpleAdapter sa = new SimpleAdapter(this, users, R.layout.taskdistributor_userentry, new String[] { "username"}, new int[] { R.id.task_userlistcheckbox});
+	    createList();
+		
+//	        ArrayList<String> names = new ArrayList<String>();
+//	        
+//	        for (HashMap<String, String> hashMap : users) 
+//	        {
+//	        	names.add(hashMap.get("name"));
+//			}
+//	        
+//	        userList = (ListView) findViewById(R.id.taskUserList);
+//	        
+//	        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.select_dialog_multichoice,names);
+//	        
+//	        userList.setAdapter(adapter);
+//	        //TODO: Alle schon auswählen
+    }
+
+	private void createList() 
+	{
+		SimpleAdapter sa = new SimpleAdapter(this, users, R.layout.taskdistributor_userentry, new String[] { "username"}, new int[] { R.id.task_userlistcheckbox});
         
         ViewBinder vb = new ViewBinder() 
         {
@@ -122,23 +108,51 @@ public class TaskDistributor extends Activity
 			}
 			
 		});
-		
-		
+	}
+
+	private void clickOnStartButton() 
+	{
+		start.setOnClickListener(new OnClickListener() 
+        {
+			
+			@Override
+			public void onClick(View v) 
+			{			
+				ListAdapter adapter = userList.getAdapter();				
+				Integer count = adapter.getCount();
+				TreeMap<String, Double> checkedUser = new TreeMap<String, Double>();
+				
+				for(int i = 0; i < count; i++)
+				{
+					View entry = userList.getChildAt(i);
+					CheckBox checkbox = (CheckBox) entry.findViewById(R.id.task_userlistcheckbox);
+					if(checkbox.isChecked())
+					{						
+						HashMap<String, String> userData = (HashMap<String, String>) adapter.getItem(i);
+						checkedUser.put(userData.get("username"), Double.valueOf(userData.get("points")));
+					}
+				}
+				
+				RandomUser rand = RandomUser.getInstance();
+				rand.setUserlist(checkedUser);
+				
+				Intent intent = new Intent(TaskDistributor.this, Create_Task.class);
+				startActivity(intent);				
+			}
+
+			
+		});
+	}
+
+	private void init() 
+	{
+		settings = getSharedPreferences(WGBuddyActivity.PREFS_NAME, 0);
         
-//	        ArrayList<String> names = new ArrayList<String>();
-//	        
-//	        for (HashMap<String, String> hashMap : users) 
-//	        {
-//	        	names.add(hashMap.get("name"));
-//			}
-//	        
-//	        userList = (ListView) findViewById(R.id.taskUserList);
-//	        
-//	        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.select_dialog_multichoice,names);
-//	        
-//	        userList.setAdapter(adapter);
-//	        //TODO: Alle schon auswählen
-    }
+        userList = (ListView) findViewById(R.id.taskUserList);
+        start = (Button) findViewById(R.id.taskGoButton);
+        user = new User(settings); 
+        users = user.get("?wgId=" + settings.getString("wg_id", ""));
+	}
 	 
 	
 }
