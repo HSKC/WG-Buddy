2b879596da75c7d86dffea3c564d305c9eb2a464
felix <felix@felix-PC.localdomain>
2012-07-06 14:45:49 +0200

    merge fehler suchen

---
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/AsyncJsonCall.java
@@ -0,0 +1,44 @@
+package de.htwg.lpn.wgbuddy;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+
+public class AsyncJsonCall extends AsyncTask<String, Void, InputStream> 
+{
+
+	@Override
+	protected InputStream doInBackground(String... params) 
+	{
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(params[0]);
+//		httppost.addHeader("Content-Length","0");
+		InputStream is = null;
+		HttpResponse response;
+		try 
+		{
+			response = httpclient.execute(httppost);
+
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+		} 
+		catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return is;
+	}
+
+}
