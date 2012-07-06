8453603f0093a8d00a85fd8878835b58020af160
felix <felix@felix-PC.localdomain>
2012-07-06 15:24:32 +0200

    Merge Fehler ausgebessert

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/UserTaskList.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/UserTaskList.java
@@ -2,19 +2,29 @@ package de.htwg.lpn.wgbuddy;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleAdapter.ViewBinder;
 import android.widget.TextView;
+import de.htwg.lpn.model.ShoppingItem;
 import de.htwg.lpn.model.Task;
+import de.htwg.lpn.wgbuddy.utility.Utilities;
 
 public class UserTaskList extends Activity 
 {
@@ -22,13 +32,16 @@ public class UserTaskList extends Activity
 	private SharedPreferences settings;
 	private Task task;
 	private ArrayList<HashMap<String, String>> tasks;
-	private ArrayList<String> idList;
 
 	 @Override
 	 public void onCreate(Bundle savedInstanceState) 
 	 {
 		 super.onCreate(savedInstanceState);
 	     setContentView(R.layout.usertasklist);
+	     
+	     settings = getSharedPreferences(WGBuddyActivity.PREFS_NAME, 0);
+         Utilities.checkByPass(this, settings);
+	     
 	     init();
 	     
 	     createList();
@@ -36,7 +49,7 @@ public class UserTaskList extends Activity
 
 	private void createList() 
 	{
-		 SimpleAdapter sa = new SimpleAdapter(this, tasks, R.layout.usertask_entry, new String[]{"name", "id"}, new int[]{R.id.usertask_entryName, R.id.textid});
+		 SimpleAdapter sa = new SimpleAdapter(this, tasks, R.layout.usertask_entry, new String[]{"name", "id"}, new int[]{R.id.usertask_entryName, R.id.usertaskEntryShowButton});
 	     
 	     ViewBinder vb = new ViewBinder() 
 	     {
@@ -50,11 +63,32 @@ public class UserTaskList extends Activity
 					name.setText((CharSequence) data);
 					return true;
 				}
-				else if(view.getId() == R.id.textid)
+				else if(view.getId() == R.id.usertaskEntryShowButton)
 				{
-					
-					idList.add(data.toString());
-					
+					final String id = data.toString();
+					Button showTaskButton = (Button) view;
+					showTaskButton.setTag(id);
+					showTaskButton.setOnClickListener(new OnClickListener() 
+					{
+						
+						@Override
+						public void onClick(View v) 
+						{
+							
+							String par = "?wgId=" + settings.getString("wg_id", "") + "&userId=" + settings.getString("user_id", "") + "&id=" + id;
+							ArrayList<HashMap<String, String>> selectedTask = task.get(par);
+							
+							Intent intent = new Intent(UserTaskList.this, UserTaskCompleteEntry.class);
+							intent.putExtra("name", selectedTask.get(0).get("name"));
+							intent.putExtra("comment", selectedTask.get(0).get("comment"));
+							intent.putExtra("createdDate", selectedTask.get(0).get("createdDate"));
+							intent.putExtra("deadline", selectedTask.get(0).get("deadline"));
+							intent.putExtra("points", selectedTask.get(0).get("points"));
+							intent.putExtra("status", selectedTask.get(0).get("status"));
+							startActivity(intent);
+						}
+
+					});
 					return true;
 				}
 				else
@@ -66,24 +100,6 @@ public class UserTaskList extends Activity
 		sa.setViewBinder(vb);
 		
 		userTaskList.setAdapter(sa);
-		userTaskList.setOnItemClickListener(new OnItemClickListener() 
-		{
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) 
-			{				
-				String id = idList.get(arg2);
-				String par = "?wgId=" + settings.getString("wg_id", "") + "&userId=" + settings.getString("user_id", "") + "&id=" + id;
-				ArrayList<HashMap<String, String>> selectedTask = task.get(par);
-			
-				Intent intent = new Intent(UserTaskList.this, UserTaskCompleteEntry.class);
-				intent.putExtra("name", selectedTask.get(0).get("name"));
-				intent.putExtra("comment", selectedTask.get(0).get("comment")); 
-				intent.putExtra("points", selectedTask.get(0).get("points"));
-				intent.putExtra("status", selectedTask.get(0).get("status"));
-				startActivity(intent);
-			}
-		});
 	}
 
 	private void init() 
@@ -93,6 +109,5 @@ public class UserTaskList extends Activity
 	     
 	    task = new Task(settings); 
 	    tasks = task.get("?wgId=" + settings.getString("wg_id", "") + "&userId=" + settings.getString("user_id", ""));
-	    idList = new ArrayList<String>();
 	}
 }
