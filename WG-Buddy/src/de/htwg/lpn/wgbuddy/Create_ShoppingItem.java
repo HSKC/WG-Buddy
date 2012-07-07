2de8a6afbe72ccfdc26c02ca861410324248dfd9
Jens <jens.kueblbeck@googlemail.com>
2012-07-07 22:59:16 +0200

    Create_ShoppingItem: ProgressDialog und ToastMessage

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/Create_ShoppingItem.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/Create_ShoppingItem.java
@@ -1,7 +1,9 @@
 package de.htwg.lpn.wgbuddy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -11,6 +13,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,6 +27,7 @@ import de.htwg.lpn.model.GoogleService;
 import de.htwg.lpn.model.ShoppingItem;
 import de.htwg.lpn.wgbuddy.utility.Dialogs;
 import de.htwg.lpn.wgbuddy.utility.Utilities;
+import de.htwg.lpn.wgbuddy.utility.WorkerThread;
 
 public class Create_ShoppingItem  extends Activity
 {
@@ -54,27 +59,52 @@ public class Create_ShoppingItem  extends Activity
 				@Override
 				public void onClick(View v) 
 				{
-					ProgressDialog.show(Create_ShoppingItem.this, "Working..", "", true, false);
-					
-					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			        nameValuePairs.add(new BasicNameValuePair("wgId", settings.getString("wg_id", "")));
-			        nameValuePairs.add(new BasicNameValuePair("userId", "0"));
-			        nameValuePairs.add(new BasicNameValuePair("name", nameEditText.getText().toString()));
-			        nameValuePairs.add(new BasicNameValuePair("comment", descriptionEditText.getText().toString()));
-			        nameValuePairs.add(new BasicNameValuePair("rating", String.valueOf(ratingBar.getRating())));
-			        nameValuePairs.add(new BasicNameValuePair("status", "0"));					
-		        
-					ShoppingItem shopping = new ShoppingItem(settings);
-					shopping.insert(nameValuePairs,Create_ShoppingItem.this);	
-					
-			        if(WGBuddyActivity.usepush)
-			        {
-						GoogleService gs = new GoogleService(settings);
-						gs.sendMessageToPhone("ShoppingItem");
-			        }
-					
-					Intent intent = new Intent(Create_ShoppingItem.this, ShoppingList.class);
-					startActivity(intent);
+					ProgressDialog pd = ProgressDialog.show(Create_ShoppingItem.this, "", getString(R.string.utilities_pleaseWait));
+					Handler handler = new Handler()
+					{
+						@Override
+						public void handleMessage(Message msg)
+						{
+							super.handleMessage(msg);
+							
+							Utilities.toastMessage(Create_ShoppingItem.this, getString(R.string.shopping_created));
+
+							Intent intent = new Intent(Create_ShoppingItem.this, ShoppingList.class);
+							startActivity(intent);
+						}
+					};
+
+					Callable<Message> callable = new Callable<Message>()
+					{
+						@Override
+						public Message call() throws Exception
+						{
+							Message message = new Message();
+
+							List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+					        nameValuePairs.add(new BasicNameValuePair("wgId", settings.getString("wg_id", "")));
+					        nameValuePairs.add(new BasicNameValuePair("userId", "0"));
+					        nameValuePairs.add(new BasicNameValuePair("name", nameEditText.getText().toString()));
+					        nameValuePairs.add(new BasicNameValuePair("comment", descriptionEditText.getText().toString()));
+					        nameValuePairs.add(new BasicNameValuePair("rating", String.valueOf(ratingBar.getRating())));
+					        nameValuePairs.add(new BasicNameValuePair("status", "0"));					
+				        
+							ShoppingItem shopping = new ShoppingItem(settings);
+							shopping.insert(nameValuePairs,Create_ShoppingItem.this);	
+							
+					        if(WGBuddyActivity.usepush)
+					        {
+								GoogleService gs = new GoogleService(settings);
+								gs.sendMessageToPhone("ShoppingItem");
+					        }
+					        
+							return message;
+						}
+
+					};
+
+					WorkerThread workerThread = new WorkerThread(callable, pd, handler);
+					workerThread.start();
 				}
     		}
         );
