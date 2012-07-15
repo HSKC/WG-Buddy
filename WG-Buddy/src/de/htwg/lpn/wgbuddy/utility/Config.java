2d4c6251b6f4cff385afa4296a2482fa705fb2a2
Jens <jens.kueblbeck@googlemail.com>
2012-07-15 22:40:39 +0200

    Standard-Config

--- WG-Buddy/src/de/htwg/lpn/wgbuddy/utility/Config.java
+++ WG-Buddy/src/de/htwg/lpn/wgbuddy/utility/Config.java
@@ -6,7 +6,7 @@ package de.htwg.lpn.wgbuddy.utility;
 public class Config
 {
 	public static final String PREFS_NAME = "WGBuddyPreferences";
-	public static final String WEBSERVER = "http://wgbuddy.domoprojekt.de/";
-	public static final String AUTH_CODE = "authCode=42cfbce07625c322c037066183d5f5c9";
+	public static final String WEBSERVER = "URL";
+	public static final String AUTH_CODE = "authCode=01234567890";
 	public static final boolean USE_PUSH = true;
 }
